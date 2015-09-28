8d1353a17ce8220a32f2e0f8fb4b7f550767f2e9
Andrew Liu <14andrewliu@gmail.com>
2015-09-28 02:02:02 -0700

    finally got foundation and sass working

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,8 +3,15 @@ import React from 'react';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        Hello from Home Component
+      <div className="row">
+
+ 					<div className="small-2 large-4 columns">
+            Hello from Home Component
+            <a href="#" className="button">Default Button</a>
+					</div>
+					<div className="small-2 large-4 columns">
+            Hello from Home Component
+          </div>
       </div>
     );
   }
--- app/stores/LiveVoterStore.js
+++ app/stores/LiveVoterStore.js
@@ -4,29 +4,30 @@ import LiveVoterActions from '../actions/LiveVoterActions';
 class LiveVoterStore {
 	constructor() {
 		this.bindActions(LiveVoterActions);
-		this.teams = {
-			red: [], 
-			blue: [], 
-			changeRecord: []
-		};
-		this.rounds = [];
-		this.officialScores = {
-			red: [], 
-			blue: [], 
-			redFinalTotal: 0, 
-			blueFinalTotal: 0
-		};
-		this.votes = {
-			red: [],
-			blue: [],
-			yes: [],
-			no: [] 
-		};
-		this.onlineUsers = 0;
-		this.title = "";
-			//keeps track and allows displaying of where the player already voted
-			//one per round, null for not voted, 1 for red, 2 for blue
+		// this.teams = {
+		// 	red: [], 
+		// 	blue: [], 
+		// 	changeRecord: []
+		// };
+		// this.rounds = [];
+		// this.officialScores = {
+		// 	red: [], 
+		// 	blue: [], 
+		// 	redFinalTotal: 0, 
+		// 	blueFinalTotal: 0
+		// };
+		// this.votes = {
+		// 	red: [],
+		// 	blue: [],
+		// 	yes: [],
+		// 	no: [] 
+		// };
+		// this.onlineUsers = 0;
+		// this.title = "";
+		// 	//keeps track and allows displaying of where the player already voted
+		// 	//one per round, null for not voted, 1 for red, 2 for blue
 		this.voted = [];
+		this.episode = undefined;
 	}
 
 	onSendVoteSuccess() {
@@ -38,11 +39,11 @@ class LiveVoterStore {
 	}
 
 	onGetUpdateSuccess(data) {
-		
+		this.episode = data;
 	}
 
-	onGetUpdateFail() {
-		toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
+	onGetUpdateFail(errorMessage) {
+		toastr.error(errorMessage);
 	}
 
 	onUpdateOnlineUsers(data) {
